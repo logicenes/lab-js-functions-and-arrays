2e3cf3797187ef31a0f88383aa3e83db3086897d
„Enes <enes.gnay@yahoo.de>
2025-06-19 17:17:15 +0200

    Finish iteration 1–5

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,45 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(a, b) {
+    return a>b ? a : b ;
+}
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    if (arr.length === 0) return null;
 
+  let word = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (word.length < arr[i].length) {
+      word = arr[i];
+    }
+  }
+  return word;
+}
 
+function longestStringReduce(arr) {
+  return arr.reduce((a, b) => a.length < b.length ? b : a, "");
+}
 
+function longestStringReduce(arr) {
+  return arr.reduce((a, b) => a.length < b.length ? b : a, "");
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr){
+    if (arr.length === 0 ) {return 0} 
+       let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+      sum= sum+ arr[i]
+  }
+  return sum;
+}
+
+
+
 
 
 
@@ -23,7 +47,17 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+     if (arr.length === 0 ){ return 0}
+     let avg=0
+     let sum=0
+     for (let i=0; i<arr.length;i++ ){
+      sum+=arr[i]
+  
+    }  avg = (sum/arr.length)
+     return avg
+} 
+
 
 
 
@@ -31,4 +65,8 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+
+  if (arr.length === 0) return null;
+  return arr.includes(word);
+}
